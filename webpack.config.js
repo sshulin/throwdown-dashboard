b5abf45062c77709f5d36ececb711d5df1b6e68f
Alexander Christphorov <hrst.work@gmail.com>
2017-11-01 01:09:39 +0700

    Fixed wrong param removal in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ function resolve (dir) {
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
